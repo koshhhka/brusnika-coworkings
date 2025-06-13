405ebfbc6debaa0464dbff348a7fcfc3745abe2d
Marat Nuriev <maratnuriev47340@gmail.com>
2025-06-13 15:59:59 +0500

    pages 4

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { store } from './store'
-import { Provider } from 'react-redux'
+// import { store } from './store'
+// import { Provider } from 'react-redux'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
-          <Provider store={store}>
-              <App />
-          </Provider>
+          {/*<Provider store={store}>*/}
+          {/*    <App />*/}
+          {/*</Provider>*/}
+          <App />
       </BrowserRouter>
   </StrictMode>,
 )
--- src/store.ts
+++ src/store.ts
@@ -1,24 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-const initialState = {
-    booking: {
-        date: null,
-        emails: [],
-        coworkingId: null,
-    },
-};
-
-const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'SET_DATE':
-            return { ...state, booking: { ...state.booking, date: action.payload } };
-        case 'ADD_EMAIL':
-            return { ...state, booking: { ...state.booking, emails: [...state.booking.emails, action.payload] } };
-        default:
-            return state;
-    }
-};
-
-export const store = configureStore({
-    reducer: rootReducer,
-});
+// import { configureStore } from '@reduxjs/toolkit';
+//
+// const initialState = {
+//     booking: {
+//         date: null,
+//         emails: [],
+//         coworkingId: null,
+//     },
+// };
+//
+// const rootReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case 'SET_DATE':
+//             return { ...state, booking: { ...state.booking, date: action.payload } };
+//         case 'ADD_EMAIL':
+//             return { ...state, booking: { ...state.booking, emails: [...state.booking.emails, action.payload] } };
+//         default:
+//             return state;
+//     }
+// };
+//
+// export const store = configureStore({
+//     reducer: rootReducer,
+// });
